9862e4a8a79fadbf4737c67b27cc554f3e8f6321
Denis Vakulenko <woof88@yandex.ru>
2022-10-15 15:29:49 +0500

    added: pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import BurgerBlock from "../components/BurgerBlock/BurgerBlock";
-import Skeleton from "../components/BurgerBlock/Skeleton";
-import Categories from "../components/Categories";
-import Sort from "../components/Sort";
+import BurgerBlock from '../components/BurgerBlock/BurgerBlock';
+import Skeleton from '../components/BurgerBlock/Skeleton';
+import Categories from '../components/Categories';
+import Pagination from '../components/Pagination/Pagination';
+import Sort from '../components/Sort';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [itemList, setItemList] = useState([]);
   const [isLoadingSkeleton, setIsloadingSkeleton] = useState(false);
-
+  const [currentPage, setCurrentPage] = useState(1);
   const [categoryId, setCategoryId] = useState(0);
-  // const [isVisible, setIsVisible] = useState(false);
   const [selectedSort, setSelectedSort] = useState({
-    name: "популярности",
-    sortProperty: "rating",
+    name: 'популярности',
+    sortProperty: 'rating',
   });
-
-  // const itemPreload = () => {
-  //   setIsloadingSkeleton(true);
-  //   api
-  //     .getBurgerList()
-  //     .then((data) => {
-  //       setItemList(data);
-  //     })
-  //     .catch((err) => `Ошибка: ${err}`)
-  //     .finally(() => setIsloadingSkeleton(false));
-  // };
-
-  // useEffect(() => {
-  //   itemPreload();
-  // }, []);
 
   useEffect(() => {
     setIsloadingSkeleton(true);
+
+    const sortBy = selectedSort.sortProperty.replace('-', '');
+    const order = selectedSort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
+
     fetch(
-      `https://631e2e919f946df7dc3f42c6.mockapi.io/items?${
-        categoryId > 0 ? `category=${categoryId}` : ""
-      }&sortBy=${selectedSort.sortProperty}&order=desc`
+      `https://631e2e919f946df7dc3f42c6.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => {
         return res.json();
@@ -46,16 +35,11 @@ const Home = () => {
       })
       .catch((err) => `Ошибка: ${err}`)
       .finally(() => setIsloadingSkeleton(false));
-  }, [categoryId, selectedSort]);
+  }, [categoryId, selectedSort, searchValue, currentPage]);
 
-  // const onClickCategory = (id) => {
-  //   setCategoryId(id);
-  // };
-
-  // const onClickSortList = (i) => {
-  //   setSelectedSort(i);
-  //   setIsVisible(false);
-  // };
+  const pizzas = itemList.map((burger) => (
+    <BurgerBlock key={burger.id} {...burger} />
+  ));
 
   return (
     <>
@@ -74,10 +58,9 @@ const Home = () => {
         <div className="content__items">
           {isLoadingSkeleton
             ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
-            : itemList.map((burger) => {
-                return <BurgerBlock key={burger.id} {...burger} />;
-              })}
+            : pizzas}
         </div>
+        <Pagination onChangePage={(number) => setCurrentPage(number)} />
       </div>
     </>
   );
