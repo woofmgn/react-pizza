522427fdade3c76691a43ebd985b4872d9041b2c
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-01-31 13:22:51 +0500

    feat: fix

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,11 @@ const rootElem = document.getElementById("root");
 if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
 
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
+  root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
 }
